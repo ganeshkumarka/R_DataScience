1d3f7e1e6a21edb1f84d2296cbfd817a026ede48
ganeshkumarka <ganeshkumarka03@gmail.com>
2024-07-29 22:13:05 +0530

    Data Reversal

---
+++ LabCycle1/Q10.R
@@ -0,0 +1,19 @@
+reversal <- function(input){
+  
+  if (length(input) <= 1)
+    return(input)
+  
+  return (c(reversal(input[-1]), input[1]))
+}
+
+main <- function(){
+  
+  input <- readline(prompt = "Enter space-separated elements: ")
+  input <- unlist(strsplit(input, " "))
+  
+  reversed <- reversal(input)
+  
+  cat("The reversed data is: ", paste(reversed, collapse = " "))
+}
+
+main()
